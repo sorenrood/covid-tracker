d859ce2fe86009b991b1803672ddbc252a57e1d9
Soren Rood <sorenrood@gmail.com>
2020-09-27 19:29:57 -0700

    added a test to make sure cases list is the same as dates list

--- tracker.py
+++ tracker.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 from lxml import html
+import sys
 
 # Constant URL that we scrape
 url = 'https://spu.edu/administration/health-services/covid-19-cases'
@@ -15,15 +16,25 @@ tree = html.fromstring(page.content)
 cases = []
 dates = []
 
-# # Loop to fetch data
-# for i in range(1, 10):
-#     # object = tree.xpath('//*[@id="pageBody"]/div/p[{}]/text()'.format(i))
-#     object = tree.xpath('//*[@id="pageBody"]/div/p[{}]/text()'.format(i))
-#     cases.append(object)
+# This is the path to the div that stores the updated data
+path_to_cases = '//*[@id="pageBody"]/div/p/text()'
+path_to_dates = '//*[@id="pageBody"]/div/strong/text()'
 
-# # Output
-# for case in cases:
-#     print(case)
+# Get cases and add to cases list
+for element in tree.xpath(path_to_cases):
+    cases.append(element)
 
-for p in tree.xpath('//*[@id="pageBody"]/div/p/text()'):
-    print(p)
+# Get dates and append to dates list
+for element in tree.xpath(path_to_dates):
+    dates.append(element)
+
+# Check to make sure length is the same for both lists
+if len(cases) != len(dates):
+    sys.exit('The cases list is not the same length as the dates list. Something went wrong with parsing.')
+
+# Store the number of cases
+number_of_cases = len(cases)
+
+# Output
+for x in range(0,number_of_cases):
+    print('{}: {}'.format(dates[x], cases[x]))
